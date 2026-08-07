1d338211675bb2bdfa42bffa44cbe7074d7c46c7
arthurx4k <arthuroliver423@gmail.com>
2026-08-06 21:13:56 -0300

    Delete Backend directory

--- Backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
---INSERT INTO usuario (email, senha, cargo) VALUES ('professor@gmail.com', '123456', 'PROFESSOR');
-
---INSERT INTO professor (nome, especialidade, descricao, usuario_id) VALUES ('Professor Teste', 'Matemática', 'Professor de matemática e física', 1);
